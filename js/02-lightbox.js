608c1dd19995a2e590a647d1bb7f035267609053
DenisNikiti <den.hikitin@gmail.com>
2022-09-18 12:35:59 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+
+const galleryEl = document.querySelector(".gallery")
+const markup = CreateGallery(galleryItems)
+galleryEl.insertAdjacentHTML("beforeend", markup)
+
+function CreateGallery(galleryItems) {
+     return galleryItems.map( ({preview, original, description}) => {
+         return  ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+  </div>
+`
+     }).join("")
+
+   
+}
+let lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData:"alt",});
+
+
 console.log(galleryItems);
